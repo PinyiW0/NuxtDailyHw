51551ac7f806c7696ad677ea91a6e8bfa0b3ee41
Pinyi Wu <wpypy8@gmail.com>
2024-11-04 18:50:04 +0800

    feat: 建立頁面＋編譯 Nuxt 應用程式成靜態網站

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+</script>
 
 <template>
   <div>
     <h1>Page: 首頁</h1>
-    <p>目前路由的路徑 - route.fullPath為 :/</p>
+    <p>目前路由的路徑 - route.fullPath為 :{{ route.fullPath }}</p>
   </div>
 </template>
 
